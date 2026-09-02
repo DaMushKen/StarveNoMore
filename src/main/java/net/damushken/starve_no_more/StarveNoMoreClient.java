2a4848ca6a53522ce9c4cd0b31381b1a713e38d2
DaMushKen <fung.ollo87@gmail.com>
2026-09-02 20:06:33 +0200

    Initial commit

---
+++ src/main/java/net/damushken/starve_no_more/StarveNoMoreClient.java
@@ -0,0 +1,10 @@
+package net.damushken.starve_no_more;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StarveNoMoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
